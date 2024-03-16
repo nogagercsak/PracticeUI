76fd34021cc9067c5bfaaab3fa7036627eb1a6a7
Noga Gercsak <nogah0317@gmail.com>
2024-03-16 14:21:42 -0400

    Initial Commit

---
+++ PracticeUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PracticeUI
+//
+//  Created by Noga Gercsak on 3/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeUI/PracticeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeUIApp.swift
+//  PracticeUI
+//
+//  Created by Noga Gercsak on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
